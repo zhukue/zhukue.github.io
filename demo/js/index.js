8e0a53a0d2d6a49cb23d8db09a42abdd37a98238
Kamil Abdulatipov <terapit1@gmail.com>
2017-11-17 22:26:04 +0300

    b

--- demo/js/index.js
+++ demo/js/index.js
@@ -1,51 +1,35 @@
-// Start upload preview image
-$(".gambar").attr("src", "https://user.gadjian.com/static/images/personnel_boy.png");
-						var $uploadCrop,
-						tempFilename,
-						rawImg,
-						imageId;
-						function readFile(input) {
-				 			if (input.files && input.files[0]) {
-				              var reader = new FileReader();
-					            reader.onload = function (e) {
-									$('.upload-demo').addClass('ready');
-									$('#cropImagePop').modal('show');
-						            rawImg = e.target.result;
-					            }
-					            reader.readAsDataURL(input.files[0]);
-					        }
-					        else {
-						        swal("Sorry - you're browser doesn't support the FileReader API");
-						    }
-						}
+$('.panel').click(function() {
+  if(!$(this).hasClass('active')) {
+    var index = $(this).index();
+    $('#order').removeClass();
+    $('#order').addClass('opt'+(index+1));
+    $('#choice').get(0).selectedIndex = index;
+    $(this).siblings().addClass('hidden');
+    $(this).addClass('active');
+    $('#order').delay(800).slideToggle(400);
+  }
+});
 
-						$uploadCrop = $('#upload-demo').croppie({
-							viewport: {
-								width: 150,
-								height: 200,
-							},
-							enforceBoundary: false,
-							enableExif: true
-						});
-						$('#cropImagePop').on('shown.bs.modal', function(){
-							// alert('Shown pop');
-							$uploadCrop.croppie('bind', {
-				        		url: rawImg
-				        	}).then(function(){
-				        		console.log('jQuery bind complete');
-				        	});
-						});
+$('#back').click(function(e) {
+  $('#order').slideToggle(400);
+  var self = this;
+  setTimeout(function() {
+    $('.panel').removeClass('hidden active');
+  }, 400);
+  e.preventDefault();
+});
 
-						$('.item-img').on('change', function () { imageId = $(this).data('id'); tempFilename = $(this).val();
-																										 $('#cancelCropBtn').data('id', imageId); readFile(this); });
-						$('#cropImageBtn').on('click', function (ev) {
-							$uploadCrop.croppie('result', {
-								type: 'base64',
-								format: 'jpeg',
-								size: {width: 150, height: 200}
-							}).then(function (resp) {
-								$('#item-img-output').attr('src', resp);
-								$('#cropImagePop').modal('hide');
-							});
-						});
-				// End upload preview image
+$('#submit').click(function(e) {
+  e.preventDefault();
+});
+
+$('#quantity').on('input change', function() {
+  var qv = $('#quantity').val();
+  if(qv % 1 != 0) {
+    qv = parseInt(qv, 10);
+    if(qv == 0) qv = "";
+    qv += "½";
+  }
+  $('label[for="quantity"]').text(qv);
+  // TODO: update the price as well
+})
